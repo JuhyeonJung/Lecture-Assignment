7f5e0b8051db53f884987fa6535abe9453c2c2fa
JuhyeonJung <72389445+JuhyeonJung@users.noreply.github.com>
2022-10-28 17:47:25 +0900

    Add files via upload

---
+++ Data_Processing_Language/HW4/Answer.py
@@ -0,0 +1,12 @@
+from Module_math import *
+
+lst = [0,4,5,6,7,8,9]
+
+print("***** 첫번째 함수 결과 *****")
+func1(lst)
+print("***** 두번째 함수 결과 *****")
+func2(lst)
+print("***** 세번째 함수 결과 *****")
+func3(lst)
+print("***** 네번째 함수 결과 *****")
+func4(lst)
